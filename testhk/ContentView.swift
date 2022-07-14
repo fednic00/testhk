4697ff04a85d2d199330a1294011eab4acdd2ba9
federico <federiconicodemo2000@gmail.com>
2022-07-14 11:09:17 +0200

    Initial Commit

---
+++ testhk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testhk
+//
+//  Created by Federico Nicodemo on 14/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testhk/testhkApp.swift
@@ -0,0 +1,17 @@
+//
+//  testhkApp.swift
+//  testhk
+//
+//  Created by Federico Nicodemo on 14/07/22.
+//
+
+import SwiftUI
+
+@main
+struct testhkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
